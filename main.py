721b7ed9b1e6219c47dcbf11d1329e75121b9770
Alexander Clarke <alex-p-clarke@hotmail.com>
2023-04-11 15:27:00 -0400

    display and play!

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# import tkinter as tk
-# import plotly.express as px
-import plotly.graph_objects as go
 import networkx as nx
 import librosa
 import os # for reading directories
 import re # for regex
+import webbrowser
+from pyvis.network import Network
+
+
 
 # ----- Settings ----- #
 hit_lib = "/Library/Audio/Sounds/Drum Kits"
@@ -59,80 +60,13 @@ for f in hit_files:
 
 # Add the edges to the graph.
 
-# Calculate the positions for the nodes.
-pos = nx.spring_layout(G, k = 0.5, iterations = 100)
-for n, p in pos.items():
-  G.nodes[n]['pos'] = p
 
-# Calculate the positions of the edges.
-edge_x = []
-edge_y = []
-for edge in G.edges():
-  x0, y0 = G.nodes[edge[0]]['pos']
-  x1, y1 = G.nodes[edge[1]]['pos']
-  edge_x.append(x0)
-  edge_x.append(x1)
-  edge_x.append(None)
-  edge_y.append(y0)
-  edge_y.append(y1)
-  edge_y.append(None)
 
-edge_trace = go.Scatter(
-  x=edge_x, y=edge_y,
-  line=dict(width=0.5, color='#888'),
-  hoverinfo='none',
-  mode='lines')
+out_filename = 'net.html'
 
-node_x = []
-node_y = []
-for node in G.nodes():
-  x, y = G.nodes[node]['pos']
-  node_x.append(x)
-  node_y.append(y)
-
-node_trace = go.Scatter(
-  x = node_x, y = node_y,
-  mode = 'markers',
-  hoverinfo = 'text',
-  marker = dict(
-    showscale = False,
-    # colorscale options
-    #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-    #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-    #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-    colorscale = 'YlGnBu',
-    reversescale = True,
-    color = [],
-    size = 10,
-    # colorbar=dict(
-    #     thickness=15,
-    #     title='Node Connections',
-    #     xanchor='left',
-    #     titleside='right'
-    # ),
-    line_width=2
-  )
-)
-
-# Set the color and hover text for each node.
-node_color = []
-node_text = []
-for (node_name, node_data) in G.nodes.items():
-  node_color.append(valid_file_types.index(node_data["ext"]))
-  node_text.append(node_name)
-
-node_trace.marker.color = node_color
-node_trace.text = node_text
-
-fig = go.Figure(data=[edge_trace, node_trace],
-  layout = go.Layout(
-    title = 'Drum Hit Similarity',
-    titlefont_size=16,
-    showlegend = False,
-    hovermode = 'closest',
-    margin = dict(b=20,l=5,r=5,t=40),
-    xaxis = dict(showgrid=False, zeroline=False, showticklabels=False),
-    yaxis = dict(showgrid=False, zeroline=False, showticklabels=False)
-  )
-)
-fig.show()
+net = Network()
+net.set_template('./template.html')
+# net.toggle_physics(False)
+net.from_nx(G)
+net.show(out_filename)
+webbrowser.open_new_tab('file:///' + os.getcwd() + '/' + out_filename)
