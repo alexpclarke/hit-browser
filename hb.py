7fa47e34ef021433db79dc2399b341fb7baea83f
Alexander Clarke <alex-p-clarke@hotmail.com>
2023-04-22 15:49:23 -0400

    refactoring

--- hb.py
+++ hb.py
@@ -1,14 +1,12 @@
+import librosa
 import networkx as nx
 import numpy as np
-import librosa
 import os
-import re
-import webbrowser
 from pyvis.network import Network
-import warnings
-from scipy import stats
+import re
 import sys
-from pathlib import Path
+import warnings
+import webbrowser
 
 # ----- SETTINGS ----- #
 
@@ -16,16 +14,12 @@ valid_file_types = [".wav", ".flac"]
 out_filename = 'hit-browser.html'
 n_matches = 3
 sim_thresh = 0.98
-
+coef_duration = 2
+coef_attack = 4
 coef_pitch = 2
 n_pitch_bins = 64
-
-coef_timbre = 4
-n_timbre_bins = 32
-
-coef_duration = 2
-
-coef_attack = 4
+coef_mfcc = 4
+n_mfcc_bins = 32
 
 # ----- HELPER FUNCTIONS ----- #
 
@@ -33,6 +27,7 @@ coef_attack = 4
 def normalize_linear(arr):
   return ((arr - np.min(arr)) / np.max(arr))
 
+# Print an error message and/or how to use this program.
 def print_usage(msg):
   if (msg): print("ERROR: " + msg)
   print("USAGE: python3 main.py <path to hit library>")
@@ -40,10 +35,11 @@ def print_usage(msg):
 
 # ----- READ ARGUMENTS ----- #
 
-# Check the number of aruments.
-if (len(sys.argv) != 2): 
+# Check the number of aruments provided.
+if (len(sys.argv) != 2):
   print_usage("Invalid number of arguments.")
 
+# Check that the path is valid.
 if os.path.isdir(sys.argv[1]):
   hit_lib = os.path.abspath(sys.argv[1])
 else:
@@ -60,57 +56,60 @@ for root, dirs, files in os.walk(hit_lib, topdown = True, followlinks = True):
     # Get the file's absolute path.
     file_path = root + "/" + file
 
-    # Get the file's extension and ignore the files that dont have a valid extension.
+    # Get the file's extension and ignore the files without a valid extension.
     file_ext = re.findall("\.[a-z,0-9]+$", file)
     if len(file_ext) == 0 or file_ext[0] not in valid_file_types:
       continue
 
-    # Turn off warnings.
-    warnings.filterwarnings("ignore") 
+    warnings.filterwarnings("ignore") # Turn off warnings.
 
     # Load the file data and trim the leading and trailing silence.
     try:
       file_data_orig, file_sr = librosa.load(file_path, sr = None)
-      file_data, file_data_trim_index = librosa.effects.trim(file_data_orig, top_db = 5)
+      file_data = librosa.effects.trim(file_data_orig, top_db = 5)[0]
     except:
-      print("Failed to load: " + file_path)
+      print("WARNING: Failed to load: " + file_path)
       continue
 
     # Get the file's duration.
     file_dur = librosa.get_duration(y = file_data, sr = file_sr)
-    if (file_dur <= 0): continue
-
-    # Get the Attack (time between 0% and 100% power)
-    file_attack_i = np.argmax(np.abs(file_data))
-    file_attack = file_attack_i / file_sr
-
-    # Get the Sustain (The level at which most of the time is spent)
-    # file_sustain = stats.mode(file_data)
-
-    # Get the decay (Time between 100% power and sustain level)
-
-    # Get teh release (Amount of time between the sustain level and 0%)
+    if (file_dur <= 0):
+      continue
 
     # Calculate the mfcc of the file.
-    file_mfcc = np.array(librosa.feature.mfcc(y = file_data, sr = file_sr, n_mfcc = n_timbre_bins))
-    file_timbre_bin = np.argmax(np.mean(file_mfcc, axis = 1))
+    file_mfcc = np.array(librosa.feature.mfcc(\
+      y = file_data,\
+      sr = file_sr,\
+      n_mfcc = n_mfcc_bins\
+    ))
+    file_mfcc_bin = np.argmax(np.mean(file_mfcc, axis = 1))
 
-    # Calculate the stft of the file.
-    file_stft = np.array(librosa.stft(y = file_data, n_fft = 2 * (n_pitch_bins - 1)))
+    # Calculate the stft of the file. The largest of which is the average pitch.
+    n_fft = 2 * (n_pitch_bins - 1)
+    file_stft = np.array(librosa.stft(y = file_data, n_fft = n_fft))
     file_pitch_bin = np.argmax(np.mean(file_stft, axis = 1))
+
+    # Calculate the attack. (Time between 0% and 100% power)
+    file_attack = np.argmax(np.abs(file_data)) / file_sr
+
+    # Calculate the sustain. (The level at which most of the time is spent)
+
+    # Calculate the decay. (Time between 100% power and sustain level)
+
+    # Calculate the release. (Amount of time between the sustain level and 0%)
 
     warnings.resetwarnings() # Turn warnings back on.
 
     # Add the file to the list of files.
     hit_data.append({
-      "name": file,
       "path": file_path[len(hit_lib):],
       "dur": file_dur,
       "attack": file_attack,
       "pitch": file_pitch_bin,
-      "timbre": file_timbre_bin
+      "mfcc": file_mfcc_bin
     })
 
+# Make sure that there were some hit loaded.
 if (len(hit_data) == 0):
   print_usage("No hits found.")
 
@@ -128,16 +127,16 @@ attack_dist = normalize_linear(abs(hit_attack - hit_attack.reshape(-1, 1)))
 hit_pitch = np.array([file['pitch'] for file in hit_data])
 pitch_dist = normalize_linear(abs(hit_pitch - hit_pitch.reshape(-1, 1)))
 
-# Compare timbre.
-hit_timbre = np.array([file['timbre'] for file in hit_data])
-timbre_dist = normalize_linear(abs(hit_timbre - hit_timbre.reshape(-1, 1)))
+# Compare mfcc.
+hit_mfcc = np.array([file['mfcc'] for file in hit_data])
+mfcc_dist = normalize_linear(abs(hit_mfcc - hit_mfcc.reshape(-1, 1)))
 
 # Calculate the similarity of the two beats.
 total_dist = \
   (coef_duration * hit_dur_dist) + \
   (coef_attack * attack_dist) + \
   (coef_pitch * pitch_dist) + \
-  (coef_timbre * timbre_dist)
+  (coef_mfcc * mfcc_dist)
 similarity = 1 - normalize_linear(total_dist)
 np.fill_diagonal(similarity, 0)
 
@@ -148,27 +147,24 @@ hit_graph = nx.Graph()
 
 # Add the nodes to the graph.
 for (i, hit_file) in enumerate(hit_data):
-  hit_graph.add_node(
-    i,
-    label = None,
-    path = hit_file['path'],
-    size = 40
-  )
+  hit_graph.add_node(i, path = hit_file['path'], size = 40)
 
 # Connect each hit with its n most similar hits.
 for i, p in enumerate([np.argsort(s) for s in similarity]):
-  for j in range(1, n_matches + 1):
-    sim = similarity[i][int(p[-j])]
+  for j in range(n_matches):
+    sim = similarity[i][int(p[-(j + 1)])]
     if sim < sim_thresh: continue
-    # if (i < int(p[-j])): hit_graph.add_edge(i, int(p[-j]))
-    hit_graph.add_edge(i, int(p[-j]), value=sim)
+    hit_graph.add_edge(i, int(p[-(j + 1)]), value=sim)
 
+# Print out the number of nodes and edges.
 print(hit_graph)
 
 # ----- CREATE PYVIZ NETWORK ----- #
 
+# Initialize the graph.
 net = Network()
 
+# Set the options for the graph.
 net.set_template('./template.html')
 net.set_options("""
 var options = {
@@ -206,14 +202,15 @@ var options = {
   }
 }
 """)
-
 net.heading = """
 {
   "root_path": "%s"
 }
 """ % (hit_lib)
 
+# Import the nodes from the NetworkX graph.
 net.from_nx(hit_graph)
 
+# Write the graph to an HTML file and open it.
 net.show(out_filename)
 webbrowser.open('file:///' + os.getcwd() + '/' + out_filename)
