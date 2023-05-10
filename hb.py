6bece63ab3158e778105af8b68891ae3ea5b325f
Alexander Clarke <alex-p-clarke@hotmail.com>
2023-05-10 16:36:50 -0400

    mostly working

--- hb.py
+++ hb.py
@@ -1,43 +1,44 @@
 import librosa
 import networkx as nx
+import matplotlib.pyplot as plt
 import numpy as np
 import os
-from pyvis.network import Network
 import re
 import sys
 import warnings
 import webbrowser
+from pyvis.network import Network
+from sklearn.decomposition import PCA
+from sklearn.neighbors import kneighbors_graph
+
 
 # ----- SETTINGS ----- #
 
-valid_file_types = [".wav", ".flac"]
+valid_types = [".wav", ".flac"]
 out_filename = 'hit-browser.html'
 n_matches = 2
-sim_thresh = 0.98
-n_mfcc_bins = 32
+tremolo_wsize = 8
+fit_degree = 1
+plot = False
+stabilization_iterations = 50
 
-# Weights of each characteristic being compared.
-coef = {
-  "duration": 1,
-  "attack":   3,
-  "decay":    2,
-  "sustain":  1,
-  "release":  0,
-  "pitch":    2,
-  "mfcc":     1
-}
+
+
 
 # ----- HELPER FUNCTIONS ----- #
 
-# Linearly normalize any data to between 0 and 1
+# Linearly normalize any data to between 0 and 1.
 def normalize_linear(arr):
-  return ((arr - np.min(arr)) / np.max(arr))
+  return ((arr - max(np.min(arr), 0)) / np.max(arr))
 
 # Print an error message and/or how to use this program.
 def print_usage(msg):
   if (msg): print("ERROR: " + msg)
   print("USAGE: python3 hb.py <path to hit library>")
   quit()
+
+
+
 
 # ----- READ ARGUMENTS ----- #
 
@@ -47,159 +48,329 @@ if (len(sys.argv) != 2):
 
 # Check that the path is valid.
 if os.path.isdir(sys.argv[1]):
-  hit_lib = os.path.abspath(sys.argv[1])
+  hit_dir = os.path.abspath(sys.argv[1])
 else:
   print_usage("Not a valid path.")
 
+
+
+
 # ----- LOAD FILE DATA -----#
 
+print('loading files...')
+
 # Inititialize the array that will store the data about each hit file.
-hit_data = []
+hits = np.array([])
 
 # Find the path to all the valid audio files.
-for root, dirs, files in os.walk(hit_lib, topdown = True, followlinks = True):
+for root, dirs, files in os.walk(hit_dir, topdown = True, followlinks = True):
   for file in files:
     # Get the file's absolute path.
-    file_path = root + "/" + file
+    path = root + "/" + file
 
     # Get the file's extension and ignore the files without a valid extension.
-    file_ext = re.findall("\.[a-z,0-9]+$", file)
-    if len(file_ext) == 0 or file_ext[0] not in valid_file_types:
+    ext = re.findall("\.[a-z,0-9]+$", file)
+    if len(ext) == 0 or ext[0] not in valid_types:
       continue
 
-    warnings.filterwarnings("ignore") # Turn off warnings.
+    # Turn off warnings.
+    warnings.filterwarnings("ignore")
 
-    # Load the file data and trim the leading and trailing silence.
+    # Load the file as a floating point time series (time x amplitude) and trim
+    # the leading and trailing silence.
     try:
-      file_data_orig, file_sr = librosa.load(file_path, sr = None)
-      file_data = librosa.effects.trim(file_data_orig, top_db = 5)[0]
+      data_orig, sr = librosa.load(path, sr = None)
+      data = librosa.effects.trim(data_orig, top_db = 5)[0]
     except:
-      print("WARNING: Failed to load: " + file_path)
+      print("WARNING: Failed to load: " + path)
       continue
 
-    # Get the file's duration.
-    file_dur = librosa.get_duration(y = file_data, sr = file_sr)
-    if (file_dur <= 0):
+    # Turn warnings back on.
+    warnings.resetwarnings()
+
+    # Ignore files with no data
+    if (len(data) == 0): 
+      print("WARNING: Failed to load (no data): " + path)
       continue
-
-    # Calculate the mfcc of the file.
-    file_mfcc = np.array(librosa.feature.mfcc(\
-      y = file_data,\
-      sr = file_sr,\
-      n_mfcc = n_mfcc_bins\
-    ))
-    file_mfcc_bin = np.argmax(np.mean(file_mfcc, axis = 1))
-
-    warnings.resetwarnings() # Turn warnings back on.
-
-    # Calculate the STFT of the file and the total power over time.
-    file_stft = np.array(librosa.stft(y = file_data, n_fft = 255, hop_length = 1))
-
-    # Determine the average pitch of the file.
-    file_pitch_bin = np.argmax(np.mean(file_stft, axis = 1))
-
-    # Calculate the attack. (Time between 0% and 100% power)
-    pow_over_time = np.sum(file_stft, axis = 0)
-    file_max_pow = np.max(pow_over_time)
-    file_attack = np.argmax(pow_over_time)
-
-    # Calculate the sustain. (The level at which most of the time is spent)
-    file_sustain_level = np.mean(pow_over_time[file_attack:])
-    sustain_margin = file_max_pow * 0.2
-
-    # Calculate the decay. (Time between 100% power and sustain level)
-    file_decay = np.argmax(pow_over_time[file_attack:] < file_sustain_level + sustain_margin)
-
-    # Calculate the release. (Amount of time between the sustain level and 0%)
-    file_sustain = np.argmax(pow_over_time[file_attack + file_decay:] < file_sustain_level - sustain_margin)
-    file_release = len(pow_over_time[file_attack + file_decay + file_sustain:])
 
     # Add the file to the list of files.
-    hit_data.append({
-      "path": file_path[len(hit_lib):],
-      "dur": file_dur,
-      "attack": file_attack / file_sr,
-      "decay": file_decay / file_sr,
-      "sustain": file_sustain / file_sr,
-      "release": file_release / file_sr,
-      "pitch": file_pitch_bin,
-      "mfcc": file_mfcc_bin
+    hits = np.append(hits, {
+      "path": path[len(hit_dir):],
+      "data": data,
+      "sr": sr,
+      "props": []
     })
 
-# Make sure that there were some hit loaded.
-if (len(hit_data) == 0):
+# Make sure that there were some hits loaded.
+if (len(hits) == 0):
   print_usage("No hits found.")
+
+
+
+
+# ----- DURATION ----- #
+
+print('calculating durations...')
+
+# Set the file's duration in samples.
+for hit in hits:
+  hit['duration'] = len(hit['data'])
+
+# Normalize and weight the values.
+hit_duration = np.array([hit['duration'] / hit['sr'] for hit in hits])
+hit_duration = normalize_linear(hit_duration)
+for (i, hit) in enumerate(hits):
+  hit['props'].append(hit_duration[i])
+
+
+
+
+# ----- PITCH ----- #
+
+print('analyzing pitches...')
+
+framesize = 256
+
+# Calculate the STFT of the file and power over time.
+for hit in hits:
+  hit['stft'] = np.abs(librosa.stft(
+    y = hit['data'],
+    n_fft = framesize,
+    hop_length = 1
+  ))
+
+  hit['power'] = librosa.amplitude_to_db(
+    np.sum(hit['stft'], axis = 0),
+    ref = np.max
+  )
+  hit['power'] = hit['power'] - np.min(hit['power'])
+
+# Determine the average pitch of the file.
+# (The pitch bin with the highest average power)
+for hit in hits:
+  total_magnitude = np.sum(hit['stft'], axis = 0)
+  total_magnitude[total_magnitude == 0] = 1
+  bin_weights = hit['stft'] / total_magnitude
+  bin_vals = (np.arange(int(framesize / 2) + 1)[:, np.newaxis] + 1)
+  pitches = np.sum(bin_weights * bin_vals, axis = 0) - 1
+  pitches = pitches
+
+  hit['pitch_line'] = np.polyfit(
+    np.arange(len(pitches)) / hit['sr'],
+    pitches,
+    fit_degree,
+    full = True
+  )
+
+# Normalize the values.
+residuals = np.array([hit['pitch_line'][1][0] for hit in hits])
+residuals = np.max(residuals) - residuals
+residuals = normalize_linear(residuals)
+
+for i in range(fit_degree + 1):
+  c_i = np.array([hit['pitch_line'][0][i] for hit in hits])
+  c_i = normalize_linear(c_i) * (2 ** (fit_degree - i))
+  for (j, hit) in enumerate(hits):
+    if i == 0:
+      hit['props'].append(c_i[j] * residuals[j])
+    else:
+      hit['props'].append(c_i[j])
+
+
+
+
+# ----- ATTACK ----- #
+
+print('calculating attack...')
+
+# Calculate the attack. (Time between 0% and 100% power)
+for hit in hits:
+  hit['attack'] = np.argmax(hit['power'])
+
+# Normalize and weight the values.
+hit_attack = np.array([hit['attack'] / hit['sr'] for hit in hits])
+hit_attack = normalize_linear(hit_attack)
+for (i, hit) in enumerate(hits):
+  hit['props'].append(hit_attack[i])
+
+
+
+
+# ----- TREMOLO ----- #
+
+print('calculating tremolo...')
+
+for hit in hits:
+  # Find all the peaks.
+  hit['peaks'] = [0]
+  i = 0
+  while (i < len(hit['data'])):
+    win_min = max(i - tremolo_wsize, 0)
+    win_max = min(i + tremolo_wsize + 1, len(hit['data']))
+    peak_window = hit['power'][win_min : win_max]
+    if (np.argmax(peak_window) == i - win_min):
+      range_min = hit['power'][hit['peaks'][-1] : i + 1].min()
+      range_max = hit['power'][hit['peaks'][-1] : i + 1].max()
+      if (range_max - range_min > 2):
+        hit['peaks'].append(i)
+      elif (hit['power'][hit['peaks'][-1]] < hit['power'][i]):
+        hit['peaks'].append(i)
+    i += 1
+  hit['peaks'].append(i - 1)
+
+  # The gap between peaks.
+  hit['peak_diffs'] = np.ediff1d(np.array(hit['peaks']) / hit['sr'])
+
+  # If there are not enough gaps to approximate
+  if len(hit['peak_diffs']) < fit_degree + 1:
+    hit['tremolo_line'] = None
+    continue
+
+  # Relationship between the gap in peaks.
+  hit['tremolo_line'] = np.polyfit(
+    np.arange(len(hit['peak_diffs'])), 
+    hit['peak_diffs'], 
+    fit_degree, 
+    full = True
+  )
+
+# Normalize the values.
+residuals = np.array([hit['pitch_line'][1][0] for hit in hits])
+residuals = np.max(residuals) - residuals
+residuals = normalize_linear(residuals)
+
+for i in range(fit_degree + 1):
+  c_i = np.array([(hit['tremolo_line'][0][i] if hit['tremolo_line'] else 0) for hit in hits])
+  c_i = normalize_linear(c_i) * (2 ** (fit_degree - i))
+  # c_i = normalize_linear(c_i)
+  for (j, hit) in enumerate(hits):
+    if i == 0:
+      hit['props'].append(c_i[j] * residuals[j])
+    else:
+      hit['props'].append(c_i[j])
+
+
+
+# ----- PLOT ANALYSIS ----- #
+
+# Plot hit.
+def plot_hit(hit, filename):
+  fig, axs = plt.subplots(3)
+  time_ticks = np.arange(0, hit['duration'] + 1, hit['sr'] / 100)
+
+  # ----- Plot 1 ----- #
+
+  axs[0].plot(np.arange(hit['duration'] + 1), hit['power'])
+  axs[0].set_xticks(time_ticks, time_ticks / hit['sr'])
+  axs[0].margins(x = 0)
+
+  for peak in hit['peaks']:
+    axs[0].axvline(x = peak, color = 'red')
+  
+  axs[0].plot([0, hit['attack'] / hit['sr']], [hit['power'][0], np.max(hit['power'])], color = 'green')
+
+  # ----- Plot 2 ----- #
+
+  axs[1].plot(np.arange(len(hit['peak_diffs'])), hit['peak_diffs'], 'o')
+  if (hit['tremolo_line']):
+    x = np.linspace(0, len(hit['peak_diffs']))
+    y = 0
+    for (i, c) in enumerate(hit['tremolo_line'][0]):
+      y += c * (x ** (fit_degree - i))
+    axs[1].plot(x, y, '-r', label='y=2x+1')
+
+  # ----- Plot 3 ----- #
+
+  axs[2].pcolormesh(hit['stft'])
+  axs[2].set_xticks(time_ticks, time_ticks / hit['sr'])
+  axs[2].set_yticks([0, framesize / 2 + 1], [0, int(hit['sr'] / 2000)])
+  axs[2].axis(ymin = 0, ymax = framesize / 2 + 1)
+  if (hit['pitch_line']):
+    x = np.linspace(0, (hit['duration'] + 1))
+    y = 0
+    for (i, c) in enumerate(hit['pitch_line'][0]):
+      y += c * (x ** (fit_degree - i))
+    axs[2].plot(x, y, '-r', label='y=2x+1')
+
+  fig.suptitle(hit['path'])
+  plt.savefig('plots/' + filename + '.png')
+  plt.close()
+
+if (plot):
+  print('generating plots...')
+  for (i, hit) in enumerate(hits):
+    plot_hit(hit, str(i))
+
+
+
 
 # ----- CALCULATE EDGES ----- #
 
-# Compare durations.
-hit_dur = np.array([hit['dur'] for hit in hit_data])
-hit_dur_dist = abs(hit_dur - hit_dur.reshape(-1, 1))
+print('calculating edges...')
 
-# Compare attack.
-hit_attack = np.array([file['attack'] for file in hit_data])
-attack_dist = abs(hit_attack - hit_attack.reshape(-1, 1))
+# Calculate the position of the nodes.
+hit_props = np.array([hit['props'] for hit in hits])
+pca = PCA(n_components = 2)
+hit_pos = pca.fit_transform(hit_props)
 
-# Compare decay.
-hit_decay = np.array([file['decay'] for file in hit_data])
-decay_dist = abs(hit_decay - hit_decay.reshape(-1, 1))
+# Calculate the position edges.
+hit_matches = kneighbors_graph(
+  hit_pos,
+  n_neighbors = n_matches,
+  mode = 'distance'
+).toarray()
 
-# Compare sustain.
-hit_sustain = np.array([file['sustain'] for file in hit_data])
-sustain_dist = abs(hit_sustain - hit_sustain.reshape(-1, 1))
 
-# Compare release.
-hit_release = np.array([file['release'] for file in hit_data])
-release_dist = abs(hit_release - hit_release.reshape(-1, 1))
 
-# Compare pitch.
-hit_pitch = np.array([file['pitch'] for file in hit_data])
-pitch_dist = abs(hit_pitch - hit_pitch.reshape(-1, 1))
-
-# Compare mfcc.
-hit_mfcc = np.array([file['mfcc'] for file in hit_data])
-mfcc_dist = abs(hit_mfcc - hit_mfcc.reshape(-1, 1))
-
-# Calculate the similarity of the two beats.
-total_dist = \
-  (coef["duration"] * normalize_linear(hit_dur_dist)) + \
-  (coef["attack"] * normalize_linear(attack_dist)) + \
-  (coef["decay"] * normalize_linear(decay_dist)) + \
-  (coef["sustain"] * normalize_linear(sustain_dist)) + \
-  (coef["release"] * normalize_linear(release_dist)) + \
-  (coef["pitch"] * normalize_linear(pitch_dist)) + \
-  (coef["mfcc"] * normalize_linear(mfcc_dist))
-
-similarity = 1 - normalize_linear(total_dist)
-np.fill_diagonal(similarity, 0)
 
 # ----- CREATE NETWORKX NETWORK ----- #
+
+print('creating graph...')
 
 # Set up the graph.
 hit_graph = nx.Graph()
 
 # Add the nodes to the graph.
-for (i, hit_file) in enumerate(hit_data):
-  hit_graph.add_node(i, path = hit_file['path'], size = 40)
+for (i, hit_file) in enumerate(hits):
+  hit_graph.add_node(i, path = hit_file['path'], size = 10)
 
-# Connect each hit with its n most similar hits.
-for i, p in enumerate([np.argsort(s) for s in similarity]):
-  for j in range(n_matches):
-    sim = similarity[i][int(p[-(j + 1)])]
-    if sim < sim_thresh: continue
-    hit_graph.add_edge(i, int(p[-(j + 1)]), value=sim)
 
-for (i, n) in enumerate(nx.spring_layout(hit_graph)):
-  print(n[0])
-  # print(hit_graph.nodes[i])
-  # print(hit_graph.nodes[i]["path"])
-  # hit_graph.nodes[i]["x"] = n[i]
-
+# Add the edges to the graph.
+for (i_1, matches) in enumerate(hit_matches):
+  for (i_2, dist) in enumerate(matches):
+    if (dist):
+      hit_graph.add_edge(i_1, i_2, weight = (np.max(hit_matches) - dist) / 4)
 
 # Print out the number of nodes and edges.
 print(hit_graph)
 
+
+
+
+# ----- STABILIZE NETWORK ----- #
+
+print('stabilizing graph...')
+
+stable_positions = nx.spring_layout(
+  hit_graph,
+  dim = 2,
+  k = 0.3,
+  pos = {i: pos for (i, pos) in enumerate(zip(hit_pos[:,0], hit_pos[:,1]))},
+  iterations = stabilization_iterations,
+  weight = 'weight',
+  scale = 1000
+)
+
+for (node, nodedata) in hit_graph.nodes.items():
+  hit_graph.nodes[node]['x'] = stable_positions[node][0]
+  hit_graph.nodes[node]['y'] = stable_positions[node][1]
+
+
+
+
 # ----- CREATE PYVIZ NETWORK ----- #
+
+print('rendering graph...')
 
 # Initialize the graph.
 net = Network()
@@ -220,25 +391,7 @@ var options = {
     "dragNodes": false
   },
   "physics": {
-    "enabled": false,
-    "solver": "barnesHut",
-    "timestep": 0.5,
-    "repulsion": {
-      "centralGravity": 0.00,
-      "springLength": 200,
-      "springConstant": 0.04,
-      "nodeDistance": 100,
-      "damping": 0.09
-    },
-    "barnesHut": {
-      "theta": 0.5,
-      "gravitationalConstant": -3000,
-      "centralGravity": 0.0,
-      "springLength": 150,
-      "springConstant": 0.05,
-      "damping": 0.09,
-      "avoidOverlap": 0
-    }
+    "enabled": false
   }
 }
 """)
@@ -246,11 +399,11 @@ net.heading = """
 {
   "root_path": "%s"
 }
-""" % (hit_lib)
+""" % (hit_dir)
 
 # Import the nodes from the NetworkX graph.
 net.from_nx(hit_graph)
 
 # Write the graph to an HTML file and open it.
-# net.show(out_filename)
-# webbrowser.open('file:///' + os.getcwd() + '/' + out_filename)
+net.show(out_filename)
+webbrowser.open('file:///' + os.getcwd() + '/' + out_filename)
